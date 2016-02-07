2761b4f33724a8bff3d314da48f526a20f370d79
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2016-02-07 10:07:55 +0000

    First commit!! :)
    Including just a basic project template of a asp.net mvc application

---
+++ src/Emergencies/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace Emergencies.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
